9a1996a27f503a45b3389c22050dca6968db0c12
chris <rubber_neck@hotmail.co.uk>
2025-04-18 09:40:55 +0100

    putting map leader at the top

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,12 +1,12 @@
-vim.loader.enable()
-
-local cmd = vim.cmd
-local opt = vim.o
-
 -- <leader> key. Defaults to `\`. Some people prefer space.
 -- The default leader is '\'. Some people prefer <space>. Uncomment this if you do, too.
 vim.g.mapleader = ' '
 vim.g.maplocalleader = ' '
+
+vim.loader.enable()
+
+local cmd = vim.cmd
+local opt = vim.o
 
 -- See :h <option> to see what the options do
 
