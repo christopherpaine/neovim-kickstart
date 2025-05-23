fba9529395b8bd561f5c09c3ae349e4cfe945222
installer <installer@gmail.com>
2025-05-23 10:04:56 +0100

    vim visual multi

--- nix/neovim-overlay.nix
+++ nix/neovim-overlay.nix
@@ -30,9 +30,10 @@ with final.pkgs.lib; let
   all-plugins = with pkgs.vimPlugins; [
     # plugins from nixpkgs go in here.
     # https://search.nixos.org/packages?channel=unstable&from=0&size=50&sort=relevance&type=packages&query=vimPlugins
+    vim-visual-multi
     bufferline-nvim
     undotree
-    auto-session
+#   auto-session
     nvim-notify
     nvim-treesitter.withAllGrammars
     luasnip # snippets | https://github.com/l3mon4d3/luasnip/
