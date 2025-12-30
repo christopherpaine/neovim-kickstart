15fb370cde8e19568350eecc6c4c1fa64726895b
installer <installer@gmail.com>
2025-12-30 22:33:07 +0000

    .

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,8 +1,5 @@
 vim.loader.enable()
 
--- Make Lua require() look in ~/.config/nvim-dev/lua
-local rtp_lua = vim.fn.expand('~/.config/nvim-dev/lua')
-package.path = rtp_lua .. '/?.lua;' .. rtp_lua .. '/?/init.lua;' .. package.path
 
 
 
