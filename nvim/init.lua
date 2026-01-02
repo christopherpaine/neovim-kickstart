812ca45bdf3a317922e8fc3922216858cf0a6763
installer <installer@gmail.com>
2026-01-02 22:12:05 +0000

    .

--- nvim/init.lua
+++ nvim/init.lua
@@ -488,7 +488,7 @@ function AddNewFigRow()
   local new_id = string.format("F%03d", num)
 
   -- Append new row with empty fields
-  vim.api.nvim_buf_set_lines(buf, -1, -1, false, {new_id .. ",,"})
+  vim.api.nvim_buf_set_lines(buf, -1, -1, false, {new_id .. ","})
 
   -- Move cursor to new line after ID
   local new_line = #lines
@@ -500,8 +500,36 @@ vim.keymap.set('n', '<Leader>mr', AddNewFigRow, {desc = "Add new fig_id row"})
 
 
 
+function add_next_fig_id()
+  local line = vim.api.nvim_get_current_line()
+  local prefix, ids_str, suffix = line:match("(.-%()(['%w,]*)(%))")
+  if not ids_str then return end
+
+  -- collect numeric parts
+  local max_num = 0
+  for id in ids_str:gmatch("'F(%d+)'") do
+    local num = tonumber(id)
+    if num > max_num then max_num = num end
+  end
+
+  -- next ID
+  local next_id = string.format("'F%03d'", max_num + 1)
+
+  -- append it to the list
+  local new_ids_str
+  if ids_str:match("%S") then
+    new_ids_str = ids_str .. "," .. next_id
+  else
+    new_ids_str = next_id
+  end
+
+  -- reconstruct line
+  local new_line = prefix .. new_ids_str .. suffix
+  vim.api.nvim_set_current_line(new_line)
+end
 
 
+vim.keymap.set('n', '<Leader>ma', add_next_fig_id, {desc = "Add next fig_id within the brackets"})
 
 
 
