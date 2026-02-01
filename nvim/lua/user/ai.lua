1c51992533193a3e3a0fc4a6bd0cd17d591f95d0
installer <installer@gmail.com>
2026-02-01 10:36:44 +0000

    .

---
+++ nvim/lua/user/ai.lua
@@ -0,0 +1,60 @@
+local pickers = require("telescope.pickers")
+local finders = require("telescope.finders")
+local actions = require("telescope.actions")
+local action_state = require('telescope.actions.state')
+local items = { "reconcile_reserve", "Item 2", "test1" }
+local conf = require('telescope.config').values
+
+
+-- I am storing stuff in ~/.chats/
+
+
+
+
+
+
+
+
+
+function _G.choose_item(on_choice)
+  pickers.new({}, {
+    prompt_title = "Choose an Item",
+    finder = finders.new_table {
+      results = items,
+    },
+    sorter = conf.generic_sorter({}),
+    attach_mappings = function(prompt_bufnr)
+      actions.select_default:replace(function()
+        actions.close(prompt_bufnr)
+        local selection = action_state.get_selected_entry()
+        on_choice(selection[1])  -- ← hand it back
+      end)
+      return true
+    end,
+  }):find()
+end
+
+-- Function to prompt and insert text
+function _G.prompt_and_insert()
+  choose_item(function(chatitem)
+  vim.ui.input({ prompt = "Insert text: " }, function(input)
+  local cmd2 = "sgpt --no-md --chat " .. chatitem .. " " .. vim.fn.shellescape(input)
+  local output = vim.fn.system(cmd2)
+  local lines = vim.split(output, "\n", { plain = true })
+
+
+    if output then
+      vim.api.nvim_put(lines, "l", true, true)
+    end
+  end)
+end)
+end
+
+
+vim.keymap.set("n", "<leader>bv", prompt_and_insert, { desc = "Prompt and insert text" })
+
+
+-- Keybind (change <leader>i if you want)
+vim.keymap.set("n", "<leader>bb", prompt_and_insert, { desc = "Prompt and insert text" })
+
+
---
+++ nvim/lua/user/key-files.lua
@@ -0,0 +1,24 @@
+local wk = require("which-key")
+
+wk.register({
+  i = {
+    name = "files",
+  },
+}, { prefix = "<leader>" })
+
+
+-- Function to open a file in a new buffer
+local function OpenFile(filepath)
+  vim.cmd('edit ' .. filepath)
+end
+
+-- Keybind
+vim.keymap.set("n", "<leader>ii", function()
+  OpenFile('/home/chris-jakoolit/neovim-kickstart/nvim/init.lua')
+end, { desc = "init lua" })
+
+ 
+
+vim.keymap.set("n", "<leader>ic", function()
+  OpenFile('/home/chris-jakoolit/.chats/chats.csv')
+end, { desc = "ai chats" })
