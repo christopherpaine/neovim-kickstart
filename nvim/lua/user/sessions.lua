a533c587aa86960d2b9b74803ee2ee95dd603976
installer <installer@gmail.com>
2026-03-14 18:25:55 +0000

    .

--- nvim/lua/user/sessions.lua
+++ nvim/lua/user/sessions.lua
@@ -1,3 +1,9 @@
+local pickers = require("telescope.pickers")
+local finders = require("telescope.finders")
+local actions = require("telescope.actions")
+local action_state = require('telescope.actions.state')
+local conf = require('telescope.config').values
+local items = {}
 
 require("which-key").add({
   { "<leader>o", group = "obsession session" },
@@ -15,3 +21,26 @@ end
 
 vim.api.nvim_set_keymap('n', '<leader>ot', ':lua toggle_obsession()<CR>', { noremap = true, silent = true })
 
+
+
+
+function getVimFiles(folderPath)
+    local p = io.popen('ls "'..folderPath..'"')
+    for file in p:lines() do
+        if file:match("%.vim$") then
+            table.insert(items, file)
+        end
+    end
+    p:close()
+    return items
+end
+
+-- Example usage:
+-- local vimFiles = getVimFiles("/path/to/folder")
+-- for _, file in ipairs(vimFiles) do
+--     print(file)
+-- end
+
+
+
+vim.api.nvim_set_keymap('n', '<leader>os', ':lua getVimFiles("/home/chris-jakoolit/christopherpaine_org/_sessions")<CR>', { noremap = true, silent = true })
