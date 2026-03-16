68b3ddec47c61c4ba4eacff4cc15b6ed96337bac
installer <installer@gmail.com>
2026-03-16 08:31:04 +0000

    .

--- nvim/lua/user/oil.lua
+++ nvim/lua/user/oil.lua
@@ -1,3 +1,13 @@
-vim.api.nvim_create_user_command('Oil', function()
-  vim.cmd('terminal oil')
-end, {})
+--vim.api.nvim_create_user_command('Oil', function()
+--  vim.cmd('terminal oil')
+--end, {})
+
+
+
+
+function oil_open()
+    vim.cmd('Obsession')
+end
+
+
+vim.api.nvim_set_keymap('n', '<leader>oo', oil_open(), { noremap = true, silent = true, desc = "oil" })
