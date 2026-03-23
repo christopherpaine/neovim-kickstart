28f327f9073909c9342538d5afc500cf5e7bb085
installer <installer@gmail.com>
2026-03-23 19:28:34 +0000

    .

--- nvim/lua/user/oil.lua
+++ nvim/lua/user/oil.lua
@@ -3,6 +3,8 @@
 --end, {})
 
 
+--Why this is needed
+--The file explorer plugin oil.nvim opens buffers that are not listed by default, so this autocmd ensures they appear in your buffer list (useful for buffer switching plugins).
 vim.api.nvim_exec([[
   autocmd FileType oil setlocal buflisted
 ]], false)
